dbae0394c48c6309ccd70e71764d6f3b77ab59a1
vishruthdevan <vishruthdevan@gmail.com>
2022-03-02 20:05:18 +0530

    Threshold bounding box of each contour

--- img_input.py
+++ img_input.py
@@ -18,3 +18,21 @@ cv2.imshow("Edges detected: ", edges)
 
 cnts, _ = cv2.findContours(edges.copy(), cv2.RETR_EXTERNAL,
                            cv2.CHAIN_APPROX_SIMPLE)
+
+
+cnts = sort_contours(cnts, method="left-to-right")[0]
+chars = []
+i = 0
+
+for c in cnts:
+    (x, y, w, h) = cv2.boundingRect(c)
+    # if (w >= 5 and w <= 150) and (h >= 15 and h <= 120):
+    roi = greyed[y:y + h, x:x + w]
+    thresh = cv2.threshold(roi, 0, 255,
+                           cv2.THRESH_BINARY_INV | cv2.THRESH_OTSU)[1]
+    (tH, tW) = thresh.shape
+
+    if tW > tH:
+        thresh = imutils.resize(thresh, width=32)
+    else:
+        thresh = imutils.resize(thresh, height=32)
