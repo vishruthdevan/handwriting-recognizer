812e80fdabb09c00d4e801112ddee860562b1f33
vishruthdevan <vishruthdevan@gmail.com>
2022-03-15 22:44:20 +0530

    refactor: structure image processing steps into procedures

--- img_input.py
+++ img_input.py
@@ -38,48 +38,62 @@ def sort_contours(cnts):
     return final
 
 
-# image_file = input("Enter path to image: ")
-image_file = "images/img2.jpg"
-image = cv2.imread(image_file)
-greyed = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-blurred = cv2.GaussianBlur(greyed, (5, 5), 0)
-edges = cv2.Canny(blurred, 30, 150)
-cv2.imshow("Edges detected: ", edges)
+def get_file():
+    # image_file = input("Enter path to image: ")
+    image_file = "images/img2.jpg"
+    image = cv2.imread(image_file)
+    return image
+
+def process_image(image):
+    greyed = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    blurred = cv2.GaussianBlur(greyed, (5, 5), 0)
+    edges = cv2.Canny(blurred, 30, 150)
+    return edges, greyed, blurred
 
 
-cnts, _ = cv2.findContours(edges.copy(), cv2.RETR_EXTERNAL,
-                           cv2.CHAIN_APPROX_SIMPLE)
-cnts = sort_contours(cnts)
+def find_letters(edges, greyed, blurred):
+    cnts, _ = cv2.findContours(edges.copy(), cv2.RETR_EXTERNAL,
+                            cv2.CHAIN_APPROX_SIMPLE)
+    cnts = sort_contours(cnts)
+    final = []
+    i = 0
+    _, _, avg_h, avg_w = cv2.boundingRect(cnts[0][0])
+    
+    for c in cnts:
+        (x, y, w, h) = cv2.boundingRect(c[0])
+
+        if (w >= avg_w - 60 and w <= avg_w + 60) and (h >= avg_h - 60 and h <= avg_h + 60):
+            avg_w = (avg_w + w) / 2
+            avg_h = (avg_h + h) / 2
+
+            roi = greyed[y:y + h, x:x + w]
+            thresh = cv2.threshold(roi, 0, 255,
+                                cv2.THRESH_BINARY_INV | cv2.THRESH_OTSU)[1]
+            (tH, tW) = thresh.shape
+
+            if tW > tH:
+                thresh = imutils.resize(thresh, width=28)
+            else:
+                thresh = imutils.resize(thresh, height=28)
+
+            dX = int(max(0, 32 - tW) / 2.0)
+            dY = int(max(0, 32 - tH) / 2.0)
+
+            padded = cv2.copyMakeBorder(thresh, top=dY, bottom=dY,
+                                        left=dX, right=dX, borderType=cv2.BORDER_CONSTANT,
+                                        value=(0, 0, 0))
+            padded = cv2.resize(padded, (28,28))
+            padded = np.expand_dims(padded, axis=-1)
+            final.append(padded)
+
+    return final
+            # cv2.imwrite(f"results/{str(i)}.png", padded)
 
 
-chars = []
-i = 0
-_, _, avg_h, avg_w = cv2.boundingRect(cnts[0][0])
-for c in cnts:
-    (x, y, w, h) = cv2.boundingRect(c[0])
-
-    if (w >= avg_w - 60 and w <= avg_w + 60) and (h >= avg_h - 60 and h <= avg_h + 60):
-        avg_w = (avg_w + w) / 2
-        avg_h = (avg_h + h) / 2
-
-        roi = greyed[y:y + h, x:x + w]
-        thresh = cv2.threshold(roi, 0, 255,
-                               cv2.THRESH_BINARY_INV | cv2.THRESH_OTSU)[1]
-        (tH, tW) = thresh.shape
-
-        if tW > tH:
-            thresh = imutils.resize(thresh, width=32)
-        else:
-            thresh = imutils.resize(thresh, height=32)
-
-        dX = int(max(0, 32 - tW) / 2.0)
-        dY = int(max(0, 32 - tH) / 2.0)
-
-        padded = cv2.copyMakeBorder(thresh, top=dY, bottom=dY,
-                                    left=dX, right=dX, borderType=cv2.BORDER_CONSTANT,
-                                    value=(0, 0, 0))
-        padded = cv2.resize(padded, (28,28))
-        # padded=np.expand_dims(padded,axis=-1)
-
-        padded = np.expand_dims(padded, axis=-1)
-        cv2.imwrite(f"results/{str(i)}.png", padded)
+def main():
+    image = get_file()
+    edges, greyed, blurred = process_image(image)
+    final = find_letters(edges, greyed, blurred)
+    # for i in final:
+    #     print(np.array([i[:5, :5, 0]]))
+    return final
