2a6230e272ba424e0df34c756351859922cc2de6
vishruthdevan <vishruthdevan@gmail.com>
2022-03-16 19:26:31 +0530

    misc: util script to check images in the dataset

---
+++ model_eval.py
@@ -0,0 +1,17 @@
+import pandas as pd
+import numpy as np
+import cv2
+
+
+data = pd.read_csv(r"data.csv").astype('float32')
+
+X = data.drop('0',axis = 1)
+y = data['0']
+
+x = np.reshape(X.values, (X.shape[0], 28,28))
+
+for i in range(80, 90):
+    cv2.imshow(f'{i}', x[i])
+    cv2.waitKey(0)
+
+print(x.shape)
